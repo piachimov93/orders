0908c1682fb149bff9c054cd29f8800be7d12afa
piachimov <nadjabrco@gmail.com>
2024-04-26 12:52:00 +0300

    .gitignore is now working

--- target/classes/db/migration/V1__create_t_order_table.sql
+++
@@ -1,7 +0,0 @@
-CREATE TABLE t_table (
-    id int not null AUTO_INCREMENT PRIMARY KEY,
-    order_number varchar(255) default null,
-    sku_code varchar(255),
-    price decimal(10,2),
-    quantity int(11)
-);
